7c97987283834beb405d3d4123eb552f5a57b3fa
Gabriel Ullmann <ullmanng162@gmail.com>
2024-04-29 22:43:12 -0400

    restore easter eggs and add more

--- js/main.js
+++ js/main.js
@@ -1,35 +1,7 @@
-var counter = 0;
-
-function init() {
-    setMobileMenu()
-    setListeners()
-}
-
-function setMobileMenu() {
-    var icon = document.querySelector(".icon-file");
-    var navbar = document.querySelector(".container-fluid .navbar");
-    var defaultNavbarClass = "navbar row";
-    if (icon && navbar) {
-        icon.addEventListener("click", function () {
-            if (navbar.className == defaultNavbarClass) {
-                navbar.className = defaultNavbarClass + " open";
-            } else {
-                navbar.className = defaultNavbarClass;
-            }
-        });
-    }
-}
-
-function setListeners() {
-    var elm = document.querySelector(".icon-floppy");
-    if (elm) {
-        elm.addEventListener("dblclick", function () {
-            counter++;
-            if (counter > 3) {
-                window.location = atob('c3BhY2UtaW52YWRlcnMuaHRtbA==');
-            }
-        });
-    }
-}
-
-init();
+const myImage = document.querySelector("#me")
+myImage.addEventListener('dblclick', () => {
+    myImage.src = "img/other/me2.jpg"
+    document.querySelector("#topics").addEventListener('dblclick', () => {
+        window.location = "space-invaders.html"
+    })
+})
