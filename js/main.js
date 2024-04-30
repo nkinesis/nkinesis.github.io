45f96072eaeabb7806d4eb0887266ff17cf2f560
Gabriel Ullmann <ullmanng162@gmail.com>
2024-04-29 23:01:07 -0400

    add date easter egg

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 const myImage = document.querySelector("#me")
+const date = new Date()
+const day = date.getDate()
+const month = date.getMonth()
+
+// Em 21 de agosto de 1996 a produção do Chevrolet Monza foi encerrada, totalizando 857.810 vendas em território brasileiro
+if (day === 21 || month === 8) {
+    myImage.src = "img/other/me2.jpg"
+}
+
 myImage.addEventListener('dblclick', () => {
     myImage.src = "img/other/me2.jpg"
     document.querySelector("#topics").addEventListener('dblclick', () => {
