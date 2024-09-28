2273099078745243b07e1a79e37ebbe9411f9b5a
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2024-09-28 18:27:50 -0300

    prevent JS error outside home

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-const myImage = document.querySelector("#me")
-const date = new Date()
-const day = date.getDate()
-const month = date.getMonth()
+const myImage = document.querySelector("#me");
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
 
 // Em 21 de agosto de 1996 a produção do Chevrolet Monza foi encerrada, totalizando 857.810 vendas em território brasileiro
 if (day === 21 && month === 8) {
-    myImage.src = "img/other/me2.jpg"
+  myImage.src = "img/other/me2.jpg";
 }
 
-myImage.addEventListener('dblclick', () => {
-    myImage.src = "img/other/me2.jpg"
-    document.querySelector("#topics").addEventListener('dblclick', () => {
-        window.location = "space-invaders.html"
-    })
-})
+if (myImage) {
+  myImage.addEventListener("dblclick", () => {
+    myImage.src = "img/other/me2.jpg";
+    document.querySelector("#topics").addEventListener("dblclick", () => {
+      window.location = "space-invaders.html";
+    });
+  });
+}
